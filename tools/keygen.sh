4a4c7cab58890da71112eef09fbd7f241d8f905a
HurryUpDude <1275139573@qq.com>
2024-01-30 04:39:40 +0800

    Modify the keygen.sh

--- tools/keygen.sh
+++ tools/keygen.sh
@@ -5,4 +5,24 @@
 
 source_dir=~/source/config
 
+echo "正在生成密钥"
+
+# 生成本机密钥
+ssh-keygen
+
+# 将生成的密钥传输给自己
+ssh-copy-id locahost
+
+# 先同步给namenode
+ssh-copy-id namenode
+
+# 从source中获取到当前的节点数量
+node_num=$( cat $source_dir | grep "datanum-")
+node_num=${node_num//datanum-/}
+for(( i=1;i<${node_num}+1;i++ ))
+do
+	node_name=datanode$i
+	ssh-copy-id $node_name
+done
+
 
